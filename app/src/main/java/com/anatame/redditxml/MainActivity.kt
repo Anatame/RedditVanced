ad088341da60dbfbff414f35c3368aa060df86b4
Anatame <anata.me97@gmail.com>
2021-11-14 19:41:41 +0530

    added navigation drawer toggle from toolbar

--- app/src/main/java/com/anatame/redditxml/MainActivity.kt
+++ app/src/main/java/com/anatame/redditxml/MainActivity.kt
@@ -4,7 +4,10 @@ import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.activity.viewModels
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.anatame.redditxml.Presentation.Adapters.HomeRecyclerViewAdapter
@@ -13,6 +16,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.flow.collect
 
 class MainActivity : AppCompatActivity() {
+    lateinit var toggle: ActionBarDrawerToggle
     private val viewModel: MainActivityViewModel by viewModels()
     private val context: Context = this@MainActivity
 
@@ -20,15 +24,39 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        setSupportActionBar(materialToolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeButtonEnabled(true)
+
+
+        navView.setNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.mItem1 -> Toast.makeText(context, "Clicked item 1", Toast.LENGTH_SHORT).show()
+                R.id.mItem2 -> Toast.makeText(context, "Clicked item 2", Toast.LENGTH_SHORT).show()
+                R.id.mItem3 -> Toast.makeText(context, "Clicked item 3", Toast.LENGTH_SHORT).show()
+            }
+
+            true
+        }
+
+
         homeRecyclerView.layoutManager = LinearLayoutManager(this)
         lifecycleScope.launchWhenStarted {
             viewModel.state.collect{ it ->
                 homeRecyclerView.adapter = HomeRecyclerViewAdapter(it.posts, context)
             }
         }
+    }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(toggle.onOptionsItemSelected(item)){
+            return true
+        }
 
-
-
+        return super.onOptionsItemSelected(item)
     }
 }
